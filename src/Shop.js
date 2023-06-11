ba7bb4147c32515165f0e4d38218f8a7d3d3630c
Pixel <petngalan@gmail.com>
2023-06-11 17:26:11 +0200

    Render the products using an array

--- src/Shop.js
+++ src/Shop.js
@@ -4,87 +4,69 @@ import uniqid from "uniqid";
 function Shop({ cart, setCart }) {
   const products = [
     {
-      id: "p-" + { uniqid },
+      id: "p-" + uniqid(),
       name: "Blue Square",
       price: "20.49",
     },
+    {
+      id: "p-" + uniqid(),
+      name: "Orange Square",
+      price: "16.99",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Green Square",
+      price: "9.99",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Red Square",
+      price: "50.01",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Purple Square",
+      price: "32.17",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Brown Square",
+      price: "27.51",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Yellow Square",
+      price: "3.15",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Pink Square",
+      price: "41.25",
+    },
+    {
+      id: "p-" + uniqid(),
+      name: "Gold Square",
+      price: "420.69",
+    },
   ];
+
+  console.log(products);
 
   return (
     <>
       <h1>Squares for sell</h1>
-      <Product
-        id={"p-" + uniqid()}
-        name="Blue Square"
-        imgLink=""
-        price="20.49"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Orange Square"
-        imgLink=""
-        price="16.99"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Green Square"
-        imgLink=""
-        price="9.99"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Red Square"
-        imgLink=""
-        price="50.01"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Purple Square"
-        imgLink=""
-        price="32.17"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Brown Square"
-        imgLink=""
-        price="27.51"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Yellow Square"
-        imgLink=""
-        price="3.15"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Pink Square"
-        imgLink=""
-        price="41.25"
-        cart={cart}
-        setCart={setCart}
-      />
-      <Product
-        id={"p-" + uniqid()}
-        name="Gold Square"
-        imgLink=""
-        price="420.69"
-        cart={cart}
-        setCart={setCart}
-      />
+      {products.map((p) => {
+        return (
+          <Product
+            id={p.id}
+            key={p.id}
+            name={p.name}
+            price={p.price}
+            cart={cart}
+            setCart={setCart}
+          />
+        );
+      })}
     </>
   );
 }
